d14c65848a6ea7a237685bc58dcb4b2dcacfeece
Andres2498 <helan2498@gmail.com>
2023-08-02 12:23:05 -0500

    Nueva ruta a crear trabajo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Home () {
-    return(
-        <div><h1>ESTOY EN EL HOME</h1></div>
+export default function Home() {
+    return (
+        <div>
+            <h1>ESTOY EN EL HOME</h1>
+
+            <Link to = "CreateWork">
+            <button> Publicar un trabajo </button>
+            </Link>
+        </div>
     )
 }
