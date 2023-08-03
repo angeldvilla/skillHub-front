58f2aacdcb8b2ba7339f25c89d1cc080496499e2
ETorresSacha <eriktorressacha@gmail.com>
2023-08-03 17:58:15 -0500

    actualizando el home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,31 @@
+import React, { useEffect } from "react";
+import Card from "./Card";
+import { useDispatch, useSelector } from "react-redux";
+import { getWork } from "../../toolkit/thunks";
+import Filters from "../filters/Filters";
+
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import skillHub from "../../assets/skillHub.jpg";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const { filterWork, work, isLoading } = useSelector((state) => state.work);
+
+  useEffect(() => {
+    dispatch(getWork());
+  }, []);
+
   return (
     <div className="relative justify-center h-screen font-mono">
       <Header />
-      <div className="flex justify-center items-center mt-8 mb-80 h-10 bg-blue-900 gap-80">
+      <Filters/>
+      <span>Loading:{isLoading ? 'True' : 'False'}</span>
+
+      
+            
+             
+      {/* <div className="flex justify-center items-center mt-8 mb-80 h-10 bg-blue-900 gap-80">
         <div className="gap-5 justify-center items-center space-x-2 p-20">
           <select className="w-32 h-auto rounded-full items-center justify-center text-white">
             <option value="" className="text-white italic">
@@ -59,7 +78,7 @@ export default function Home() {
             ENTER
           </button>
         </div>
-      </div>
+      </div> */}
 
       <div className="absolute left-0 top-52 w-64 p-px-10 border-3 text-white text-center text-1xl">
         <p className="bg-blue-900 mb-6 w-full">
@@ -93,7 +112,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-wrap justify-around ml-64 ">
+      {/* <div className="flex flex-wrap justify-around ml-64 ">
         <div className="justify-around m-64 rounded-md grid grid-cols-4 gap-32 -mt-64">
           <div className="bg-white p-4 rounded-lg shadow-lg w-52">
             <img src={skillHub} className="rounded-full w-32" />
@@ -175,8 +194,8 @@ export default function Home() {
             </button>
           </div>
         </div>
-      </div>
-
+      </div> */}
+      <Card work={work}/>
       <Footer />
     </div>
   );
