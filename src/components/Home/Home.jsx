836b99e3c7ca460a66959f147e431534d1438f4f
ETorresSacha <eriktorressacha@gmail.com>
2023-08-04 00:30:38 -0500

    paginado

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getWork } from "../../toolkit/thunks";
 
@@ -6,10 +6,27 @@ import Header from "../Header/Header";
 import Card from "./Card";
 import Filters from "../filters/Filters";
 import Footer from "../Footer/Footer";
+import Paginated from "../Paginated/Paginated";
 
 export default function Home() {
   const dispatch = useDispatch();
   const { work, isLoading } = useSelector((state) => state.work);
+  const numberOfWorks= work.length
+
+
+
+  //paginado
+  const [index,setIndex]=useState(0) // se crea este estado dentro de "HomePage" con la finalidad de pasarlo por props al componente "Nav" y utilizarlo, pero el uso principal de este hook es en el componente "Paginado"
+    
+  const [workForPage,setWorkForPage] = useState(3)
+  const [page,setPage] = useState(1) 
+
+  const inicio = (page - 1) * workForPage;
+
+  const final = inicio + workForPage;
+
+  const works = work.slice(inicio, final);
+  //----------
 
   useEffect(() => {
     dispatch(getWork());
@@ -20,7 +37,23 @@ export default function Home() {
       <Header />
       <Filters />
       <span className="ml-64">Loading:{isLoading ? "True" : "False"}</span>
-      <Card work={work} />
+      <Paginated 
+        numberOfWorks={numberOfWorks}
+        workForPage={workForPage}
+        page={page}
+        setPage={setPage}
+        index={index}
+        setIndex={setIndex}
+      />
+      <Card work={works} />
+      <Paginated 
+        numberOfWorks={numberOfWorks}
+        workForPage={workForPage}
+        page={page}
+        setPage={setPage}
+        index={index}
+        setIndex={setIndex}
+      />
       <Footer />
     </div>
   );
--- src/toolkit/slice.js
+++ src/toolkit/slice.js
@@ -83,3 +83,56 @@ export const {
 } = workSlice.actions;
 
 //export default workSlice.reducer
+
+
+
+
+
+// import React from 'react'
+// import './paginado.css'
+
+
+
+// function Paginado({numberOfRecets,page,recipeForPage,setPage, index, setIndex}) {
+
+//     const pageNumber=[]
+//     for(let i=1;i<=Math.ceil(numberOfRecets/recipeForPage);i++){
+//         pageNumber.push(i)
+//     }
+
+//     const numBotones = 5 // este es el numero de botones que queremos renderizar
+//     //todo--> FUNCIONA PARA UN NUMERO DE BOTONES IMPAR, SI SE QUIERE PONER PARA UN NUMERO PAR SE TIENE QUE MODIFICAR LA LOGICA EN EL CSS DEL BOTON "NEXT" (numBotones-1)
+
+//     const pagePrevius =()=>{
+
+//         setIndex(index-numBotones)
+//         if(page%numBotones===0) setPage(numBotones*(Math.trunc(page/numBotones)-2)+1)
+//         else setPage(numBotones*(Math.trunc(page/numBotones)-1)+1)
+//     }
+
+//     const pageNext=()=>{
+//         setIndex(index+numBotones)
+//         const suma=page+numBotones
+//         if(suma%numBotones===0) setPage(numBotones*((suma/numBotones)-1)+1)
+//         else setPage(numBotones*(Math.trunc(suma/numBotones))+1) 
+//     }
+
+//     const specificPage=(page)=>{
+//         setPage(page)
+//     }
+
+
+//   return (
+//     <div className='conteiner-paginado'>
+//             <button className={`${page<=numBotones?'atras-adelante-desactive':'atras-adelante'}`} onClick={pagePrevius} disabled={page<=1}>{"<<"}</button>
+//             {pageNumber.map(noPage=>(
+//                 <button key={noPage}  onClick={()=>specificPage(noPage)} className={`${noPage===page ? 'btn-page-active':'btn-page'}`}>{noPage}</button>
+//             )).slice(index,index+numBotones)}
+//             <button   className={`${pageNumber.length%numBotones===0 && page>=pageNumber.length-(numBotones-1) ? 'atras-adelante-desactive':(page>=(numBotones*Math.trunc(pageNumber.length/numBotones)+1)?'atras-adelante-desactive':'atras-adelante')}`} onClick={pageNext} disabled={page>=pageNumber.length}>{">>"}</button>
+//     </div>
+//   )
+// }
+
+// export default Paginado
+
+
