03a8a069440622492246482337baac9197df41a8
NoistNT <arielgnr23@gmail.com>
2023-08-04 12:24:23 -0300

    add route for jobdetail

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import LandingPage from "./components/LandingPage/LandingPage";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Home from "./components/Home/Home";
+import JobDetail from "./components/JobDetail/JobDetail";
 /* ------------------------------------------- */
 import { Routes, Route } from "react-router-dom";
 /* ------------------------------------------- */
@@ -11,9 +12,10 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/home" element={<Home /> } /> 
+      <Route path="/home" element={<Home />} />
       <Route path="/signin" element={<Login />} />
       <Route path="/signup" element={<Register />} />
+      <Route path="/jobdetail" element={<JobDetail />} />
     </Routes>
   );
 }
