f9776c4a0f831e6490edce646a20664bd461388d
ETorresSacha <eriktorressacha@gmail.com>
2023-08-06 19:46:00 -0500

    se modifico el search con la ruta del back

--- src/toolkit/thunks.js
+++ src/toolkit/thunks.js
@@ -1,13 +1,30 @@
-import { allWork, startIsLoading } from "./slice";
+import { allWork, getWorkName, startIsLoading } from "./slice";
 import axios from "axios";
 
 
 export const getWork =()=>{
     return async(dispatch)=>{
+        try {
+            dispatch(startIsLoading())
+
+            const {data}= await axios('http://localhost:3001/empleador') /*https://fakestoreapi.com/products*/
+            dispatch(allWork({resultWork:data}))
+            
+        } catch (error) {
+            console.log(error)
+            
+        }
+        
+        
+    }
+}
+
+export const getWorkForName =(title)=>{
+    return async(dispatch)=>{
         
         dispatch(startIsLoading())
 
-        const {data}= await axios('http://localhost:3001/empleador') /*https://fakestoreapi.com/products*/
-        dispatch(allWork({resultWork:data}))
+        const {data}= await axios(`http://localhost:3001/empleador/job?title=${title}`) /*https://fakestoreapi.com/products*/
+        dispatch(getWorkName(data))
     }
 }
