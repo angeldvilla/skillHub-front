0ea43525767b004a2fdafb7dee8fa3bd512b999b
ETorresSacha <eriktorressacha@gmail.com>
2023-08-07 16:57:20 -0500

    filter

--- src/toolkit/slice.js
+++ src/toolkit/slice.js
@@ -26,8 +26,6 @@ export const workSlice = createSlice({
       state.filterWork = action.payload.resultWork; // para el filtro por nombre
       state.filterWork2 = action.payload.resultWork; // para el filtro por precio
       state.filterWork3 = action.payload.resultWork; // para el tipode trabajo hacia precio
-      //state.filterWork4 = action.payload.resultWork; // para el tipode trabajo hacia precio
-      //state.filterWork5 = action.payload.resultWork; // para el tipode trabajo hacia precio
     },
 
     filterName: (state, action) => {
@@ -44,24 +42,24 @@ export const workSlice = createSlice({
 
         state.filterWork4.length === 0 ? typePrice = state.filterWork2 : typePrice = state.filterWork4
 
-        if (action.payload === "menos de 100$"){
-            dataPrice = typePrice.filter((element) => parseInt(element.price) < 100)
-            priceConditioned= state.filterWork3.filter((element) => parseInt(element.price) < 100)
+        if (action.payload === "menos de 200$"){
+            dataPrice = typePrice.filter((element) => parseInt(element.price) < 200)
+            priceConditioned= state.filterWork3.filter((element) => parseInt(element.price) < 200)
         }
-        else if (action.payload === "100$-200$"){
-            dataPrice = typePrice.filter((element) => parseInt(element.price) >= 100 && parseInt(element.price) <= 200)
-        priceConditioned= state.filterWork3.filter((element) => parseInt(element.price) >= 100 && parseInt(element.price) <= 200)
+        else if (action.payload === "200$-400$"){
+            dataPrice = typePrice.filter((element) => parseInt(element.price) >= 200 && parseInt(element.price) <= 400)
+        priceConditioned= state.filterWork3.filter((element) => parseInt(element.price) >= 200 && parseInt(element.price) <= 400)
 
         }
 
-        else if (action.payload === "200$-300$"){
-            dataPrice = typePrice.filter((element) => parseInt(element.price) >= 200 && parseInt(element.price) <= 300)
-            priceConditioned= state.filterWork3.filter((element) => parseInt(element.price) >= 200 && parseInt(element.price) <= 300)
+        else if (action.payload === "400$-600$"){
+            dataPrice = typePrice.filter((element) => parseInt(element.price) >= 400 && parseInt(element.price) <= 600)
+            priceConditioned= state.filterWork3.filter((element) => parseInt(element.price) >= 400 && parseInt(element.price) <= 600)
         }
 
-        else if (action.payload === "400$-mas"){
-            dataPrice = typePrice.filter((element) => parseInt(element.price) > 400)
-            priceConditioned= state.filterWork3.filter((element) => parseInt(element.price) > 400)
+        else if (action.payload === "600$-mas"){
+            dataPrice = typePrice.filter((element) => parseInt(element.price) > 600)
+            priceConditioned= state.filterWork3.filter((element) => parseInt(element.price) > 600)
         }
         else {dataPrice = typePrice
               priceConditioned=typePrice
@@ -74,10 +72,9 @@ export const workSlice = createSlice({
 
     //!FILTRO POR TIPO DE TRABAJO
     filterTypeWork: (state, action) => {
-        
+
         // SE GUARDA EN UN ESTADO PARA USARLO CUANDO FILTRAMOS(COMO CONDICIONAL) POR PRECIO
-        console.log(state.work)
-        state.filterWork4 = state.filterWork3.filter(ele=>ele.ability === action.payload)
+        state.filterWork4 = state.filterWork3.filter(ele=>ele.ability.includes(action.payload))
         
         // SA HACE UNA CONDICIONAL PARA SABER QUE DATOS VAMOS A FILTRAR
         let typeWorkConditioned
@@ -90,7 +87,7 @@ export const workSlice = createSlice({
           else state.work=state.filterWork3
         }
         else{
-          action.payload === "all" ? state.work = typeWorkConditioned : state.work = typeWorkConditioned.filter((element) => element.ability === action.payload)
+          action.payload === "all" ? state.work = typeWorkConditioned : state.work = typeWorkConditioned.filter((element) => element.ability.includes(action.payload))
         }
        
     },
