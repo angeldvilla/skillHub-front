73f904c087242b0986bc2d221fc1015df67cadc1
NoistNT <arielgnr23@gmail.com>
2023-08-08 17:38:08 -0300

    remove firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBvksPhNfQCNUfd__8MlKseKjmvF8p5pVM",
@@ -15,4 +14,3 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
