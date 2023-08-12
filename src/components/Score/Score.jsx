34cddfe1d44f84445f102be2c382aeae0ee02e16
raul2216 <raulpcq22@gmail.com>
2023-08-12 18:55:56 -0500

    new component Score, install dependencias react icon

---
+++ src/components/Score/Score.jsx
@@ -0,0 +1,31 @@
+import { FaStar } from "react-icons/fa";
+import { useState } from "react";
+
+export default function Score() {
+  const [rating, setRating] = useState(0); // Corregido: Inicializar el estado con 0
+
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+        return (
+          <label key={i} className="inline-block cursor-pointer transition-colors duration-200">
+            <input
+              type="radio"
+              name="rating"
+              className="hidden"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)} // Corregido: Usar setRating correctamente
+            />
+            <FaStar
+              className={`${
+                rating >= ratingValue ? "text-yellow-500" : "text-gray-300"
+              } cursor-pointer transition-colors duration-200`}
+              size={40}
+            />
+          </label>
+        );
+      })}
+    </div>
+  );
+}
