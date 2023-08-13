f04d341caf4af91288ed30b523e30b47a65d6886
NoistNT <arielgnr23@gmail.com>
2023-08-13 16:43:38 -0300

    update filter and searchbar styles

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -44,47 +44,40 @@ export default function Filters() {
   };
 
   return (
-    <div className="flex justify-center items-center mb-1 h-10 bg-blue-900 gap-80">
-      <div className="gap-5 justify-center items-center space-x-4 p-20 ml-48">
-        <select
-          onChange={handleOrdertitle}
-          className="w-45 h-auto rounded-full items-center justify-center text-black font-bold"
-        >
-          <option className="font-bold">ORDEN</option>
-          <option value="A-Z">ASCENDENTE</option>
-          <option value="Z-A">DESCENDENTE</option>
+    <div className="flex justify-between items-center w-full bg-gray-900 text-gray-200 py-2">
+      <div className="flex flex-row gap-8 px-40">
+        <select onChange={handleOrdertitle} className="text-center rounded-md">
+          <option className="font-bold">Orden</option>
+          <option value="A-Z">Ascendente</option>
+          <option value="Z-A">Descendente</option>
         </select>
 
-        <select
-          onChange={handleFilterPrice}
-          className="w-45 h-auto rounded-full items-center justify-center text-black font-bold"
-        >
-          <option className="font-bold">TODOS</option>
-          <option value="menos de 200$">MENOR A 200</option>
+        <select onChange={handleFilterPrice} className="text-center rounded-md">
+          <option className="font-bold">Todos</option>
+          <option value="menos de 200$">Menor 200</option>
           <option value="200$-400$">200-400</option>
           <option value="400$-600$">400-600</option>
-          <option value="600$-mas">MAYOR A 600</option>
+          <option value="600$-mas">Mayor a 600</option>
         </select>
 
         <select
           onChange={handleFilterTypeWork}
-          className="w-45 h-auto rounded-full items-center justify-center text-black font-bold"
+          className="text-center rounded-md"
         >
           <option className="font-bold" value="all">
-            TODOS
+            Todos
           </option>
           {allCategory.map((element, index) => (
             <option key={index} value={element.category}>
-              {element.category.toUpperCase()}
+              {element.category}
             </option>
           ))}
         </select>
-
         <button
           onClick={resetFilters}
-          className="bg-red-600 hover:bg-red-500 text-white px-4 py-1 rounded-md inline-block shadow-md hover:shadow-lg transform transition-transform duration-200 hover:-translate-y-0.5"
+          className="bg-blue-700 hover:bg-blue-800 transition duration-150 py-1 px-3 rounded-md"
         >
-          RESET
+          Limpiar filtros
         </button>
       </div>
 
