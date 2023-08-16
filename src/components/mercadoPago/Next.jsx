a9187c18f84c8be85a17001a5c9f5ddb7d9c99d1
maiariaboypujadas <maiariaboy24@gmail.com>
2023-08-16 12:36:51 -0300

    estilos next

--- src/components/mercadoPago/Next.jsx
+++ src/components/mercadoPago/Next.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Loader from '../Loader/Loader';
 import logoSkillHub from "../../assets/skillHub.jpg";
-import { center } from '@cloudinary/url-gen/qualifiers/textAlignment';
+import { Button } from "@material-tailwind/react";
 
 //require("dotenv").config();
 
@@ -55,18 +55,18 @@ const Next = () => {
   return (
     <div className="container" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", minHeight: "100vh", backgroundColor: "black", color: "lightblue" }}>
       <img src={logoSkillHub} alt="Logo de la empresa" style={{ width: "300px", marginBottom: "20px" }} />
-      <p className="title" style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "20px", width: "80%", textAlign: "center"}}>
+      <p className="title" style={{ fontSize: "24px", fontWeight: "bold", marginBottom: "20px", width: "50%", textAlign: "center"}}>
         ¡Listo para llevar tus propuestas al mundo!
       </p>
-      <p className="description" style={{ fontSize: "18px", marginBottom: "20px", width: "80%" }}>
+      <p className="description" style={{ fontSize: "18px", marginBottom: "20px", width: "50%", textAlign: "center"}}>
         En SkillHub, nos complace brindarte una experiencia excepcional. Contáctanos ante cualquier consulta o inquietud.
       </p>
       {datos === null ? (
         <Loader />
       ) : (
-        <button className="next-button" style={{ color: "white", border: "1px solid lightblue", width: "200px" }} onClick={handleGuardarDatos}>
-          NEXT
-        </button>
+        <div>
+         <Button className = 'bg-white' variant="outlined" color='black' onClick={handleGuardarDatos}>NEXT</Button>
+         </div>
       )}
     </div>
   );
