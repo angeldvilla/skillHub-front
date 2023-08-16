85ac18c975f0b477127a2b33364d0eb115d1fc10
NoistNT <arielgnr23@gmail.com>
2023-08-16 12:12:43 -0300

    add dashboard headers

---
+++ src/utils/dashboard.js
@@ -0,0 +1,12 @@
+export const TABLE_HEAD = [
+  "Nombre",
+  "Apellido",
+  "Email",
+  "Número de teléfono",
+  "Suscripto",
+  "Plan de suscripción",
+  "Posteos realizados",
+  "Posteos disponibles",
+  "Activo",
+  "Acciones",
+];
