35685fc587f9ac5c8a6a228e265048733b885b2f
NoistNT <arielgnr23@gmail.com>
2024-06-17 16:47:21 -0300

    Add images to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = { reactStrictMode: true }
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'img.daisyui.com' }]
+  }
+}
 
 export default nextConfig
