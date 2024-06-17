9a9e363b3fa6d9e501eb8ae0310e2040c0d6f30c
angeldvilla <escarlata.2003@gmail.com>
2024-06-17 14:41:19 -0500

    Add favicon, SVG, and change font in layout, create folder styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
 
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Sora } from 'next/font/google'
+import '../styles/globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const sora = Sora({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Skillhub',
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={sora.className}>{children}</body>
     </html>
   )
 }
