d0453758289d2302b033ca4f942bf5dd4ea6d0dd
NoistNT <arielgnr23@gmail.com>
2024-06-18 15:00:26 -0300

    Add jobs list component

---
+++ modules/job/components/list.tsx
@@ -0,0 +1,34 @@
+import type { Job } from '@/modules/job/types'
+
+import Card from './card'
+
+export default function List({ jobs }: { jobs: Job[] }) {
+  return (
+    <div className="mx-auto my-4 grid w-full max-w-lg grid-cols-1 gap-5 rounded-3xl border-2 border-neutral-700 bg-neutral-950 p-8 text-neutral-500 md:max-w-4xl md:grid-cols-2 xl:max-w-7xl xl:grid-cols-3 xl:gap-6">
+      {jobs.map(
+        ({
+          id,
+          category,
+          description,
+          image,
+          location,
+          title,
+          type,
+          userId
+        }) => (
+          <Card
+            key={id}
+            category={category}
+            description={description}
+            id={id}
+            image={image}
+            location={location}
+            title={title}
+            type={type}
+            userId={userId}
+          />
+        )
+      )}
+    </div>
+  )
+}
