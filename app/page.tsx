cf3ef192518664d8de8725df278377f7e41fe513
NoistNT <arielgnr23@gmail.com>
2024-06-18 12:58:52 -0300

    Small refactor in app page

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,9 @@ import ButtonDefault from '@/modules/core/components/button'
 
 export default function Home() {
   return (
-    <div>
-      <div className="flex min-h-screen flex-col items-center justify-center">
-        <h1 className="mb-4 text-3xl font-bold">WELCOME TO SKILLHUB!!!</h1>
-        <ButtonDefault />
-      </div>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="mb-4 text-3xl font-bold">WELCOME TO SKILLHUB!!!</h1>
+      <ButtonDefault />
     </div>
   )
 }
