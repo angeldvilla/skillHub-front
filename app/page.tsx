d6d5f0037809ea30aec096db3038ab136b1dee10
angeldvilla <escarlata.2003@gmail.com>
2024-06-18 01:28:47 -0500

    Change and init design for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import ButtonDefault from '@/modules/core/components/button'
+import NavBar from '@/modules/core/components/navbar'
+
 export default function Home() {
   return (
     <div>
-      <h1 className="flex flex-col items-center">WELCOME TO SKILLHUB!!!</h1>
+      <NavBar />
+      <div className="flex min-h-screen flex-col items-center justify-center">
+        <h1 className="mb-4 text-3xl font-bold">WELCOME TO SKILLHUB!!!</h1>
+        <ButtonDefault />
+      </div>
     </div>
-  );
+  )
 }
