78d7a4e837d8114913f682c73d1dd09b7789fe03
NoistNT <arielgnr23@gmail.com>
2024-06-18 15:03:01 -0300

    Add job detail page

---
+++ app/jobs/[id]/page.tsx
@@ -0,0 +1,26 @@
+import { getJob } from '@/modules/job/actions/actions'
+
+interface Props {
+  params: { id: string }
+}
+
+export default async function Page({ params: { id } }: Props) {
+  const { category, description, image, location, title, wage, userId } =
+    await getJob(id)
+
+  return (
+    <div className="hero min-h-[92vh]">
+      <div className="hero-overlay bg-opacity-60" />
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h2 className="mb-10 text-5xl font-bold">{title}</h2>
+          <p className="mb-3">{description}</p>
+          <p className="mb-3">{category}</p>
+          <p className="mb-3">{location}</p>
+          <p className="mb-3">{wage}</p>
+          <p className="mb-3">{userId}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
