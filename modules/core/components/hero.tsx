47eb04beb2ff582634acd5e3d1af1c6a58cea61c
NoistNT <arielgnr23@gmail.com>
2024-06-20 10:05:42 -0300

    Small refactor in button and hero component

--- modules/core/components/hero.tsx
+++ modules/core/components/hero.tsx
@@ -1,18 +1,11 @@
-'use client'
-
 import Image from 'next/image'
-import Link from 'next/link'
 
 import CustomButton from './button'
 
 export default function Hero() {
-  const handleScroll = () => {
-    ;<Link href="/jobs" />
-  }
-
   return (
     <div className="relative z-0 mx-auto flex max-w-[1440px] flex-col gap-5 xl:flex-row">
-      <div className="padding-x flex-1 pt-24">
+      <div className="flex-1 px-4 pt-24">
         <h1 className="text-[50px] font-extrabold sm:text-[64px] 2xl:text-[72px]">
           ¡Welcome to SkillHub!
         </h1>
@@ -23,7 +16,6 @@ export default function Hero() {
         </p>
         <CustomButton
           containerStyles="bg-blue-700 text-white rounded-full mt-10"
-          handleClick={handleScroll}
           title="Explore Jobs"
         />
       </div>
@@ -31,14 +23,14 @@ export default function Hero() {
       <div className="flex w-full items-end justify-end xl:h-screen xl:flex-[1.5]">
         {/* hero image */}
         <div className="relative z-0 h-[590px] w-[90%] xl:h-full xl:w-full">
-          <Image fill alt="Jobs" className="object-contain" src="/hero.png" />
+          <Image fill alt="Jobs" className="object-contain" src="/hero.webp" />
           {/* hero image overlay */}
           <div className="bg-hero-bg -1/4 absolute -z-10 h-[590px] w-full overflow-hidden bg-repeat-round xl:-right-1/2 xl:-top-20 xl:h-screen">
             <Image
               fill
               alt="Jobs"
               className="object-contain"
-              src="/hero-bg.png"
+              src="/hero-bg.webp"
             />
           </div>
         </div>
