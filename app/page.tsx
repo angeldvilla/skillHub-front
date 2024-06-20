9ca2d8914c9ee89e1347ed44fb8a72863118ab89
angeldvilla <escarlata.2003@gmail.com>
2024-06-20 00:50:39 -0500

    Refactor home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import ButtonDefault from '@/modules/core/components/button'
+import Hero from '@/modules/core/components/hero'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="mb-4 text-3xl font-bold">WELCOME TO SKILLHUB!!!</h1>
-      <ButtonDefault />
-    </div>
+    <main className="overflow-hidden">
+      <Hero />
+    </main>
   )
 }
