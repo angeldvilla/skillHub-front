71118ddbb7d1f9bc0fcb69458bf6b1d1d8109d56
angeldvilla <escarlata.2003@gmail.com>
2024-06-20 00:50:58 -0500

    Refactor jobs layout

--- app/jobs/layout.tsx
+++ app/jobs/layout.tsx
@@ -1,12 +1,3 @@
-import NavBar from '@/modules/core/components/navbar'
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex h-full flex-col">
-      <div>
-        <NavBar />
-      </div>
-      {children}
-    </div>
-  )
+  return <div>{children}</div>
 }
