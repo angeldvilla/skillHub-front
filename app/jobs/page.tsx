013dc6cb17f64e3f3d37e690939aacc5add7c5fb
angeldvilla <escarlata.2003@gmail.com>
2024-06-20 00:51:32 -0500

    Refactor jobs page

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,8 +1,13 @@
 import { getJobs } from '@/modules/job/actions/actions'
 import List from '@/modules/job/components/list'
 
-export default async function Page() {
+export default async function JobsPage() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
   const jobs = await getJobs()
 
-  return <List jobs={jobs} />
+  return (
+    <div className="flex h-full flex-col">
+      <List jobs={jobs} />
+    </div>
+  )
 }
