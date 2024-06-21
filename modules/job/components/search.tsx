c782cf443bad6a7ec290c0f018687f025a013b2f
NoistNT <arielgnr23@gmail.com>
2024-06-21 15:06:56 -0300

    Add debounce to search component

--- modules/job/components/search.tsx
+++ modules/job/components/search.tsx
@@ -1,20 +1,21 @@
 'use client'
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { useDebouncedCallback } from 'use-debounce'
 
 export default function Search() {
   const pathname = usePathname()
   const { replace } = useRouter()
   const searchParams = useSearchParams()
 
-  const handleSearch = (term: string) => {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams)
 
     if (term) params.set('title', term)
     else params.delete('title')
 
     replace(`${pathname}?${params.toString()}`)
-  }
+  }, 300)
 
   return (
     <label className="input input-md input-bordered mx-auto my-4 flex max-w-xs items-center gap-2">
