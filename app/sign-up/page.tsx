0f64dafd30c02f978c41546dd0fb392840782a6c
angeldvilla <escarlata.2003@gmail.com>
2024-06-20 21:48:02 -0500

    Add sign up page

---
+++ app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUp from '@/modules/auth/components/singUp'
+
+export default async function SignUpPage() {
+  return (
+    <div className="flex h-full flex-col">
+      <SignUp />
+    </div>
+  )
+}
