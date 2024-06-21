922b9594cbb875aca98609994cafc148e528a536
angeldvilla <escarlata.2003@gmail.com>
2024-06-20 21:48:29 -0500

    Add sign up component

---
+++ modules/auth/components/singUp.tsx
@@ -0,0 +1,66 @@
+export default function SignUp() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="px-20 text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Register now!</h1>
+          <p className="py-12">
+            Register with us so you can live the experience of offering your
+            talent to the world, or find a talent with whom you genuinely
+            connect... Welcome!!!
+          </p>
+        </div>
+        <div className="card w-full max-w-sm shrink-0 bg-base-100 shadow-2xl">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label" htmlFor="name">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                required
+                className="input input-bordered"
+                placeholder="email"
+                type="email"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label" htmlFor="email">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                required
+                className="input input-bordered"
+                placeholder="email"
+                type="email"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label" htmlFor="email">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                required
+                className="input input-bordered"
+                placeholder="password"
+                type="password"
+              />
+              <label className="label" htmlFor="sign-in">
+                <a
+                  className="link-hover link label-text-alt text-blue-500"
+                  href="/sign-in"
+                >
+                  Do you have an account? Login here!
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-info text-white" type="button">
+                Register
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
