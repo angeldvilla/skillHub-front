6a711aec5d1a3cafd66fa76ea00c953c75827fee
angeldvilla <escarlata.2003@gmail.com>
2024-06-20 21:15:12 -0500

    Add index for the export all core components

---
+++ modules/core/components/index.ts
@@ -0,0 +1,7 @@
+import Loader from './loader'
+import NavBar from './navbar'
+import Footer from './footer'
+import Hero from './hero'
+import FeaturedWorks from './featuredWorks'
+
+export { Loader, NavBar, Footer, Hero, FeaturedWorks }
