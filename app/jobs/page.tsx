208376887824d6887cde4b15ce989bd99610e0b0
NoistNT <arielgnr23@gmail.com>
2024-06-21 18:51:46 -0300

    Refactor jobs page

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,5 +1,6 @@
-import { getJobs } from '@/modules/job/actions/actions'
+import { getCategories, getJobs } from '@/modules/job/actions/actions'
 import List from '@/modules/job/components/list'
+import Menu from '@/modules/job/components/menu'
 import Search from '@/modules/job/components/search'
 
 interface Props {
@@ -7,21 +8,24 @@ interface Props {
 }
 
 export default async function JobsPage({ searchParams: { title } }: Props) {
-  const jobs = await getJobs(title)
+  const jobsData = getJobs(title)
+  const categoriesData = getCategories()
 
-  if (jobs.length)
-    return (
-      <>
-        <Search />
-        <List jobs={jobs} />
-      </>
-    )
+  const [jobs, categories] = await Promise.all([jobsData, categoriesData])
 
   return (
     <>
-      <Search />
-      {/* TODO: Fix styles */}
-      <p className="my-48 text-center text-xl">No jobs found with that title</p>
+      <div className="mx-auto flex max-w-5xl items-center justify-around">
+        <Menu categories={categories} />
+        <Search />
+      </div>
+      {jobs.length ? (
+        <List jobs={jobs} />
+      ) : (
+        <p className="my-48 text-center text-xl">
+          No jobs found with that title
+        </p>
+      )}
     </>
   )
 }
