d3c5d5263a7e45d5b74f4adcaf3f98ff451a0854
angeldvilla <escarlata.2003@gmail.com>
2024-06-20 21:13:54 -0500

    Add prop btn type for custom button

--- modules/core/components/button.tsx
+++ modules/core/components/button.tsx
@@ -4,13 +4,14 @@ import Link from 'next/link'
 
 export default function CustomButton({
   title,
-  containerStyles
+  containerStyles,
+  btnType
 }: CustomButtonProps) {
   return (
     <Link href="/jobs">
       <button
         className={`relative flex flex-row items-center justify-center px-6 py-3 outline-none ${containerStyles}`}
-        type="button"
+        type={!btnType ? 'button' : 'submit'}
       >
         <span className="flex-1">{title}</span>
       </button>
