65d2aee9a7631bf551f434ef9dd153045fe5f6fd
angeldvilla <escarlata.2003@gmail.com>
2024-06-20 21:23:30 -0500

    Temporarily add 3 carrousel for different image in every one

--- modules/core/components/carrouselVertical2.tsx
+++ modules/core/components/carrouselVertical2.tsx
@@ -1,19 +1,8 @@
 import Image from 'next/image'
 
-export default function CarrouselVertical() {
+export default function CarrouselVertical2() {
   return (
     <div className="carousel carousel-vertical h-96 rounded-box border-2 border-blue-400">
-      <div className="carousel-item h-full">
-        <Image alt="img-it" height={390} src="/it.webp" width={390} />
-      </div>
-      <div className="carousel-item h-full">
-        <Image
-          alt="img-education"
-          height={390}
-          src="/education.webp"
-          width={390}
-        />
-      </div>
       <div className="carousel-item h-full">
         <Image
           alt="img-healtcare"
@@ -43,6 +32,17 @@ export default function CarrouselVertical() {
           src="/ocuppations.webp"
           width={390}
         />
+      </div>
+      <div className="carousel-item h-full">
+        <Image
+          alt="img-education"
+          height={390}
+          src="/education.webp"
+          width={390}
+        />
+      </div>
+      <div className="carousel-item h-full">
+        <Image alt="img-it" height={390} src="/it.webp" width={390} />
       </div>
     </div>
   )
--- modules/core/components/carrouselVertical3.tsx
+++ modules/core/components/carrouselVertical3.tsx
@@ -1,16 +1,27 @@
 import Image from 'next/image'
 
-export default function CarrouselVertical() {
+export default function CarrouselVertical3() {
   return (
     <div className="carousel carousel-vertical h-96 rounded-box border-2 border-blue-400">
       <div className="carousel-item h-full">
-        <Image alt="img-it" height={390} src="/it.webp" width={390} />
+        <Image alt="img-sales" height={390} src="/sales.webp" width={390} />
+      </div>
+      <div className="carousel-item h-full">
+        <Image alt="img-tourism" height={390} src="/tourism.webp" width={390} />
       </div>
       <div className="carousel-item h-full">
         <Image
           alt="img-education"
           height={390}
           src="/education.webp"
+          width={390}
+        />
+      </div>
+      <div className="carousel-item h-full">
+        <Image
+          alt="img-ocuppations"
+          height={390}
+          src="/ocuppations.webp"
           width={390}
         />
       </div>
@@ -23,24 +34,13 @@ export default function CarrouselVertical() {
         />
       </div>
       <div className="carousel-item h-full">
+        <Image alt="img-it" height={390} src="/it.webp" width={390} />
+      </div>
+      <div className="carousel-item h-full">
         <Image
           alt="img-logistic"
           height={390}
           src="/logistic.webp"
-          width={390}
-        />
-      </div>
-      <div className="carousel-item h-full">
-        <Image alt="img-sales" height={390} src="/sales.webp" width={390} />
-      </div>
-      <div className="carousel-item h-full">
-        <Image alt="img-tourism" height={390} src="/tourism.webp" width={390} />
-      </div>
-      <div className="carousel-item h-full">
-        <Image
-          alt="img-ocuppations"
-          height={390}
-          src="/ocuppations.webp"
           width={390}
         />
       </div>
