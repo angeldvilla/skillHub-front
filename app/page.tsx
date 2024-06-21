5e4266531d75312c34ae0b3dc3374b344b10835c
angeldvilla <escarlata.2003@gmail.com>
2024-06-20 21:13:00 -0500

    Add Featured works in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Hero from '@/modules/core/components/hero'
+import { FeaturedWorks, Hero } from '@/modules/core/components'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <FeaturedWorks />
     </main>
   )
 }
