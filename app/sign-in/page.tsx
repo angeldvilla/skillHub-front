3c35b97f2f5e884ac30eabc5a635e6411da4ca89
angeldvilla <escarlata.2003@gmail.com>
2024-06-20 21:47:54 -0500

    Add sign in page

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import SignIn from '@/modules/auth/components/signIn'
+
+export default async function SignInPage() {
+  return (
+    <div className="flex h-full flex-col">
+      <SignIn />
+    </div>
+  )
+}
