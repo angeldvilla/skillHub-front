2516a65504c43246a2a6261ca17ced81b96cb0e9
NoistNT <arielgnr23@gmail.com>
2024-06-24 15:51:51 -0300

    Add sfilter by category to jobs menu component

--- modules/job/components/menu.tsx
+++ modules/job/components/menu.tsx
@@ -1,16 +1,34 @@
+'use client'
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+
 import { capitalizeFirstLetter, slugify } from '@/lib/utils'
 
 interface Props {
   categories: string[]
 }
 
-export default async function Menu({ categories }: Props) {
+export default function Menu({ categories }: Props) {
+  const pathname = usePathname()
+  const { replace } = useRouter()
+  const searchParams = useSearchParams()
+
+  const handleChange = (category: string) => {
+    const params = new URLSearchParams(searchParams)
+
+    if (category && category !== 'Categories') params.set('category', category)
+    else params.delete('category')
+
+    replace(`${pathname}?${params.toString()}`)
+  }
+
   return (
-    <select className="select select-bordered w-40">
-      <option disabled defaultValue="All">
-        Categories
-      </option>
-      <option>All</option>
+    <select
+      className="select select-bordered w-40"
+      defaultValue={searchParams.get('category')?.toString() ?? ''}
+      onChange={(event) => handleChange(event.target.value)}
+    >
+      <option>Categories</option>
       {categories.map((category) => (
         <option key={category} value={category}>
           {capitalizeFirstLetter(slugify(category))}
