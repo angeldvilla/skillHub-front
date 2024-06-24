c29c049f72f8dc2da37a2e8201994824824f84ca
angeldvilla <escarlata.2003@gmail.com>
2024-06-24 15:09:03 -0500

    Export the theme controller component

--- modules/core/components/index.ts
+++ modules/core/components/index.ts
@@ -3,5 +3,6 @@ import NavBar from './navbar'
 import Footer from './footer'
 import Hero from './hero'
 import FeaturedWorks from './featuredWorks'
+import ThemeController from './themeController'
 
-export { Loader, NavBar, Footer, Hero, FeaturedWorks }
+export { Loader, NavBar, Footer, Hero, FeaturedWorks, ThemeController }
