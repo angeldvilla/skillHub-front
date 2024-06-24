e7e4cf66499f58602e3f70329b37f31e2fc87d69
angeldvilla <escarlata.2003@gmail.com>
2024-06-24 15:11:01 -0500

    Add logic for the loading view the skeleton

--- modules/job/components/list.tsx
+++ modules/job/components/list.tsx
@@ -1,11 +1,32 @@
+'use client'
+
 import type { Job } from '@/modules/job/types'
+
+import { useState, useEffect } from 'react'
 
 import { capitalizeFirstLetter, slugify } from '@/lib/utils'
 
 import Card from './card'
+import SkeletonCard from './skeletonCard'
 
 export default function List({ jobs }: { jobs: Job[] }) {
-  return (
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  return isLoading ? (
+    <div className="mx-auto mb-4 grid w-full max-w-lg grid-cols-1 gap-5 rounded-3xl border-2 border-neutral-700 bg-neutral-950 p-8 text-neutral-500 md:max-w-4xl md:grid-cols-2 xl:max-w-7xl xl:grid-cols-3 xl:gap-6">
+      {jobs.map(({ id }) => (
+        <SkeletonCard key={id} />
+      ))}
+    </div>
+  ) : (
     <div className="mx-auto mb-4 grid w-full max-w-lg grid-cols-1 gap-5 rounded-3xl border-2 border-neutral-700 bg-neutral-950 p-8 text-neutral-500 md:max-w-4xl md:grid-cols-2 xl:max-w-7xl xl:grid-cols-3 xl:gap-6">
       {jobs.map(({ id, category, description, image, location, title }) => (
         <Card
