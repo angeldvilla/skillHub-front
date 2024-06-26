d37e72561a378288495e23adc883631ec78bbb85
angeldvilla <escarlata.2003@gmail.com>
2024-06-25 19:20:28 -0500

    Fix change position of menu and add more options of filters

--- modules/job/components/menu.tsx
+++ modules/job/components/menu.tsx
@@ -23,17 +23,44 @@ export default function Menu({ categories }: Props) {
   }
 
   return (
-    <select
-      className="select select-bordered w-40"
-      defaultValue={searchParams.get('category')?.toString() ?? ''}
-      onChange={(event) => handleChange(event.target.value)}
-    >
-      <option>Categories</option>
-      {categories.map((category) => (
-        <option key={category} value={category}>
-          {capitalizeFirstLetter(slugify(category))}
-        </option>
-      ))}
-    </select>
+    <div className="flex flex-col items-center gap-10">
+      <h1 className="m-2 text-center text-3xl">Menu</h1>
+      <select
+        className="select select-bordered w-52"
+        defaultValue={searchParams.get('category')?.toString() ?? ''}
+        onChange={(event) => handleChange(event.target.value)}
+      >
+        <option>Categories</option>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {capitalizeFirstLetter(slugify(category))}
+          </option>
+        ))}
+      </select>
+      <select
+        className="select select-bordered w-52"
+        defaultValue={searchParams.get('category')?.toString() ?? ''}
+        onChange={(event) => handleChange(event.target.value)}
+      >
+        <option>Wage</option>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {capitalizeFirstLetter(slugify(category))}
+          </option>
+        ))}
+      </select>
+      <select
+        className="select select-bordered w-52"
+        defaultValue={searchParams.get('category')?.toString() ?? ''}
+        onChange={(event) => handleChange(event.target.value)}
+      >
+        <option>Location</option>
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {capitalizeFirstLetter(slugify(category))}
+          </option>
+        ))}
+      </select>
+    </div>
   )
 }
