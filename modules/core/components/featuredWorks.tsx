d69431f635907628d3920983d7a7578f353f8285
angeldvilla <escarlata.2003@gmail.com>
2024-06-26 17:12:52 -0500

    Fix responsive the carrousel´s section

--- modules/core/components/featuredWorks.tsx
+++ modules/core/components/featuredWorks.tsx
@@ -6,22 +6,20 @@ export default function FeaturedWorks() {
   return (
     <div className="relative z-0 mx-auto flex max-w-[1440px] flex-col gap-5 xl:flex-row">
       <div className="flex-1 px-4 pt-24">
-        <h1 className="text-[26px] font-extrabold sm:text-[64px] 2xl:text-[72px]">
+        <h1 className="text-[48px] font-extrabold sm:text-[64px] 2xl:text-[72px]">
           Featured Works
         </h1>
-        <p className="black-100 t-5 mb-5 text-[20px] font-light">
+        <p className="black-100 t-5 mb-5 text-[24px] font-light">
           Explore jobs you might connect with
         </p>
-        {/* hero image container */}
-        <div className="flex w-full items-end justify-end xl:h-screen xl:flex-[1.5]">
-          {/* hero image */}
-          <div className="relative z-0 h-[590px] w-[90%] xl:h-full xl:w-full">
+        <div className="flex w-full flex-col items-center justify-center gap-10 xl:h-screen xl:flex-row">
+          <div className="relative z-0 h-[400px] w-full xl:h-full xl:w-1/3">
             <CarrouselVertical />
           </div>
-          <div className="relative z-0 h-[590px] w-[90%] xl:h-full xl:w-full">
+          <div className="relative z-0 h-[400px] w-full xl:h-full xl:w-1/3">
             <CarrouselVertical2 />
           </div>
-          <div className="relative z-0 h-[590px] w-[90%] xl:h-full xl:w-full">
+          <div className="relative z-0 h-[400px] w-full xl:h-full xl:w-1/3">
             <CarrouselVertical3 />
           </div>
         </div>
