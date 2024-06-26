5cdc61c60a80fa9375d2b77fc386336e1ca903d0
angeldvilla <escarlata.2003@gmail.com>
2024-06-25 19:17:11 -0500

    Fix style in jobs page

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -18,25 +18,33 @@ export default async function JobsPage({
   const [jobs, categories] = await Promise.all([jobsData, categoriesData])
 
   return (
-    <>
-      <div className="mx-auto flex max-w-5xl items-center justify-around">
+    <div className="flex min-h-screen">
+      {/* Left column for menu */}
+      <div className="w-[240px] border-r border-neutral-700 p-4">
         <Menu categories={categories} />
-        <Search />
       </div>
-      {jobs.length ? (
-        <List jobs={jobs} />
-      ) : (
-        <p className="my-28 flex flex-col items-center gap-8 text-center text-3xl">
-          <Image
-            alt="not-found-jobs"
-            className="object-contain"
-            height={300}
-            src="/NotFoundJobs.webp"
-            width={300}
-          />
-          No jobs found with that title
-        </p>
-      )}
-    </>
+
+      {/* Right column for jobs */}
+      <div className="w-[100%] p-2">
+        <div className="mb-4">
+          <Search />
+        </div>
+
+        {jobs.length ? (
+          <List jobs={jobs} />
+        ) : (
+          <p className="my-28 flex flex-col items-center gap-8 text-center text-3xl">
+            <Image
+              alt="not-found-jobs"
+              className="object-contain"
+              height={300}
+              src="/NotFoundJobs.webp"
+              width={300}
+            />
+            No jobs found with that title
+          </p>
+        )}
+      </div>
+    </div>
   )
 }
