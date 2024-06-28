48a7f5c7e9f95e189b0889e8eae4f620ea87b6bd
angeldvilla <escarlata.2003@gmail.com>
2024-06-28 01:11:26 -0500

    Refactor in job page for server component

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,9 +1,6 @@
-import Image from 'next/image'
-
 import { getJobs } from '@/modules/job/actions/actions'
-import List from '@/modules/job/components/list'
 import Menu from '@/modules/job/components/menu'
-import Search from '@/modules/job/components/search'
+import JobsDisplay from '@/modules/job/components/jobDispaly'
 
 interface Props {
   searchParams: { title?: string; category?: string; location?: string }
@@ -15,32 +12,15 @@ export default async function JobsPage({
   const jobs = await getJobs(title, category, location)
 
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen flex-col md:flex-row">
       {/* Left column for menu */}
-      <div className="w-[240px] border-r border-neutral-700 p-4">
+      <div className="w-full border-b border-neutral-700 p-4 md:w-[240px] md:border-b-0 md:border-r">
         <Menu />
       </div>
 
       {/* Right column for jobs */}
-      <div className="w-[100%] p-2">
-        <div className="mb-4">
-          <Search />
-        </div>
-
-        {jobs.length ? (
-          <List jobs={jobs} />
-        ) : (
-          <p className="my-28 flex flex-col items-center gap-8 text-center text-3xl">
-            <Image
-              alt="not-found-jobs"
-              className="object-contain"
-              height={300}
-              src="/NotFoundJobs.webp"
-              width={300}
-            />
-            No jobs found with that title
-          </p>
-        )}
+      <div className="w-full p-2">
+        <JobsDisplay jobs={jobs} />
       </div>
     </div>
   )
