f5d425aa0d06328e8f330a75dadef3c6dec399a0
NoistNT <arielgnr23@gmail.com>
2024-07-04 13:04:44 -0300

    Remove console logs from sign up button

--- modules/auth/ui/sign-up-button.tsx
+++ modules/auth/ui/sign-up-button.tsx
@@ -5,14 +5,11 @@ import { useFormStatus } from 'react-dom'
 export default function SignUpButton() {
   const { pending } = useFormStatus()
 
-  console.log('pending', pending)
-
   return (
     <button
       aria-disabled={pending}
       className="btn btn-info text-white"
       type="submit"
-      onClick={() => console.log('Register new user')}
     >
       Register
     </button>
