500a06b7f2746e29d0b916d8065db95efafc6c7e
NoistNT <arielgnr23@gmail.com>
2024-07-04 12:45:44 -0300

    Update next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,8 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '*' }]
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack']
-    })
-
-    return config
   }
 }
 
