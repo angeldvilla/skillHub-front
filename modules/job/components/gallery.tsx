0b68b591443019b91027c0ec952aa10b30d756de
NoistNT <arielgnr23@gmail.com>
2024-08-07 11:05:45 -0300

    Add missing wage to gallery component

--- modules/job/components/gallery.tsx
+++ modules/job/components/gallery.tsx
@@ -2,7 +2,7 @@
 
 import type { Job } from '@/modules/job/types'
 
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { capitalizeFirstLetter, slugify } from '@/lib/utils'
 
@@ -28,17 +28,20 @@ export default function Gallery({ jobs }: { jobs: Job[] }) {
     </div>
   ) : (
     <div className="mx-auto mb-4 grid w-full max-w-lg grid-cols-1 gap-5 rounded-3xl border-2 border-neutral-700 p-8 text-neutral-500 md:max-w-4xl md:grid-cols-2 xl:max-w-7xl xl:grid-cols-3 xl:gap-6">
-      {jobs.map(({ id, category, description, image, location, title }) => (
-        <Card
-          key={id}
-          category={capitalizeFirstLetter(slugify(category))}
-          description={description}
-          id={id}
-          image={image}
-          location={capitalizeFirstLetter(location)}
-          title={title}
-        />
-      ))}
+      {jobs.map(
+        ({ id, category, description, image, location, title, wage }) => (
+          <Card
+            key={id}
+            category={capitalizeFirstLetter(slugify(category))}
+            description={description}
+            id={id}
+            image={image}
+            location={capitalizeFirstLetter(location)}
+            title={title}
+            wage={wage}
+          />
+        )
+      )}
     </div>
   )
 }
