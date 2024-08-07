4807e007e7c33bb6fecaf44b811ccc0644f7a162
NoistNT <arielgnr23@gmail.com>
2024-08-07 11:26:42 -0300

    Remove unnecessary  readonly in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 
+import { Footer, NavBar } from '@/modules/core/components'
 import '@/styles/globals.css'
-import { NavBar, Footer } from '@/modules/core/components'
 
 export const metadata: Metadata = {
   title: 'Skillhub',
@@ -10,9 +10,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
       <body className="relative">
