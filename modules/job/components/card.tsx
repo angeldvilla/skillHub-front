df98ba3c9ff50b1ffb6764d75287a201015e69b4
NoistNT <arielgnr23@gmail.com>
2024-08-07 16:39:11 -0300

    Refactor card component

--- modules/job/components/card.tsx
+++ modules/job/components/card.tsx
@@ -15,10 +15,8 @@ export default function Card({
   id,
   category,
   description,
-  image,
   location,
-  title,
-  wage
+  title
 }: Props) {
   return (
     <Link href={`/jobs/${id}`}>
@@ -28,11 +26,7 @@ export default function Card({
             alt="Shoes"
             className="w-full"
             height={300}
-            src={
-              !image
-                ? image
-                : 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
-            }
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
             width={300}
           />
         </figure>
@@ -42,47 +36,13 @@ export default function Card({
             <div className="badge badge-accent">NEW</div>
           </h2>
           <p>{description}</p>
-          <div className="card-actions mt-4 items-center justify-between">
-            <div className="text-md font-semibold text-neutral-400">
-              $ {wage}
+          <div className="card-actions mt-2 justify-end">
+            <div className="badge badge-outline hover:bg-base-content hover:text-base-100">
+              {category}
             </div>
-            <div className="flex gap-2">
-              <div className="badge badge-outline hover:bg-base-content hover:text-base-100">
-                {category}
-              </div>
-              <div className="badge badge-outline hover:bg-base-content hover:text-base-100">
-                {location}
-              </div>
+            <div className="badge badge-outline hover:bg-base-content hover:text-base-100">
+              {location}
             </div>
-          </div>
-          {/* rating */}
-          <div className="rating mt-4">
-            <input
-              className="mask mask-star-2 bg-orange-400"
-              name="rating-2"
-              type="radio"
-            />
-            <input
-              defaultChecked
-              className="mask mask-star-2 bg-orange-400"
-              name="rating-2"
-              type="radio"
-            />
-            <input
-              className="mask mask-star-2 bg-orange-400"
-              name="rating-2"
-              type="radio"
-            />
-            <input
-              className="mask mask-star-2 bg-orange-400"
-              name="rating-2"
-              type="radio"
-            />
-            <input
-              className="mask mask-star-2 bg-orange-400"
-              name="rating-2"
-              type="radio"
-            />
           </div>
         </div>
       </div>
