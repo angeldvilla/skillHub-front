ca06a0cd8c67dade5a350b6c3338a223ebf0643c
NoistNT <arielgnr23@gmail.com>
2024-08-12 11:52:28 -0300

    Update jobs layout

--- app/jobs/layout.tsx
+++ app/jobs/layout.tsx
@@ -1,3 +1,19 @@
+import Menu from '@/modules/job/components/menu'
+import Search from '@/modules/job/components/search'
+import { View } from '@/modules/job/components/view'
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>
+  return (
+    <div className="lg:flex lg:gap-x-10">
+      <Menu />
+      <div>
+        <h1 className="my-4 text-center text-3xl">Explore Jobs</h1>
+        <div className="flex items-center justify-center gap-x-6">
+          <Search />
+          <View />
+        </div>
+        {children}
+      </div>
+    </div>
+  )
 }
