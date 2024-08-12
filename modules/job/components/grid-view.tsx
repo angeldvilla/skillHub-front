4d88c5936b028f0690318a83a385179e48fc47d9
NoistNT <arielgnr23@gmail.com>
2024-08-12 11:55:51 -0300

    Minor style tweaks in grid-view component

--- modules/job/components/grid-view.tsx
+++ modules/job/components/grid-view.tsx
@@ -6,7 +6,7 @@ import Card from './card'
 
 export default function GridView({ jobs }: { jobs: Job[] }) {
   return (
-    <div className="mx-auto mb-4 grid w-full max-w-lg grid-cols-1 gap-5 rounded-3xl border-2 border-neutral-700 p-8 text-neutral-500 md:max-w-4xl md:grid-cols-2 xl:max-w-7xl xl:grid-cols-3 xl:gap-6">
+    <div className="mx-auto mb-4 grid w-full max-w-lg grid-cols-1 gap-5 rounded-3xl border-2 border-gray-500/50 p-4 text-neutral-500 md:max-w-4xl md:grid-cols-2 md:gap-6 xl:max-w-7xl xl:grid-cols-3 xl:gap-8 xl:p-6">
       {jobs.map(
         ({ id, category, description, image, location, title, wage }) => (
           <Card
