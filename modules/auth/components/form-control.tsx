3056f9c1d2ab703bb0a4f5e788cc4c3828d847a9
NoistNT <arielgnr23@gmail.com>
2024-08-13 11:05:05 -0300

    Add form control component

---
+++ modules/auth/components/form-control.tsx
@@ -0,0 +1,26 @@
+import { slugify } from '@/lib/utils'
+
+interface Props {
+  label: string
+  placeholder: string
+  type: string
+}
+
+export function FormControl({ label, placeholder, type }: Props) {
+  const normalizedLabel = slugify(label)
+
+  return (
+    <div className="form-control">
+      <label className="label" htmlFor={normalizedLabel}>
+        <span className="label-text">{label}</span>
+      </label>
+      <input
+        required
+        className="input input-bordered placeholder-gray-500"
+        name={normalizedLabel}
+        placeholder={placeholder}
+        type={type}
+      />
+    </div>
+  )
+}
