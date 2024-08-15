2aa1205f5dffa10702f0c50d278bdaecdfbe74a6
NoistNT <arielgnr23@gmail.com>
2024-08-15 15:12:07 -0300

    Move props to component in custom button

--- modules/core/components/button.tsx
+++ modules/core/components/button.tsx
@@ -1,17 +1,17 @@
-import type { CustomButtonProps } from '@/types'
-
 import Link from 'next/link'
 
-export default function CustomButton({
-  title,
-  containerStyles,
-  btnType,
-  route
-}: CustomButtonProps) {
+interface Props {
+  title: string
+  className?: string
+  btnType?: 'button' | 'submit'
+  route?: string
+}
+
+export default function CustomButton({ title, className, btnType }: Props) {
   return (
-    <Link href={route ? route : '/'}>
+    <Link href="/jobs">
       <button
-        className={`relative flex flex-row items-center justify-center px-6 py-3 outline-none ${containerStyles}`}
+        className={`relative flex flex-row items-center justify-center px-6 py-3 outline-none ${className}`}
         type={!btnType ? 'button' : 'submit'}
       >
         <span className="flex-1">{title}</span>
--- types/index.ts
+++
@@ -1,9 +0,0 @@
-import type { MouseEventHandler } from 'react'
-
-export interface CustomButtonProps {
-  title: string
-  containerStyles?: string
-  handleClick?: MouseEventHandler<HTMLButtonElement>
-  btnType?: 'button' | 'submit'
-  route?: string
-}
