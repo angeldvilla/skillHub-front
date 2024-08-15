b490319ede93305d20150816da5ddfa2536ba4e3
NoistNT <arielgnr23@gmail.com>
2024-08-15 14:40:54 -0300

    Add fallback urls to login component

--- modules/auth/ui/login.tsx
+++ modules/auth/ui/login.tsx
@@ -19,12 +19,12 @@ export function Login() {
       <ClerkLoaded>
         <SignedOut>
           <div className="btn btn-outline hidden w-24 items-center justify-center border-blue-500 hover:border-gray-100 hover:bg-blue-500 hover:text-white hover:duration-300 hover:ease-linear lg:flex">
-            <SignInButton mode="modal" />
+            <SignInButton fallbackRedirectUrl="/jobs" mode="modal" />
           </div>
         </SignedOut>
         <SignedIn>
           <div className="btn btn-ghost w-24">
-            <UserButton />
+            <UserButton afterSwitchSessionUrl="/" />
           </div>
         </SignedIn>
       </ClerkLoaded>
