cdd8dbd494659df380cec46416d572fc84d16fe4
NoistNT <arielgnr23@gmail.com>
2024-08-15 14:44:14 -0300

    Add new job form component

---
+++ modules/job/components/new-job-form.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import { useActionState } from 'react'
+
+import { capitalizeFirstLetter, slugify } from '@/lib/utils'
+import { FormControl } from '@/modules/auth/components/form-control'
+import { SubmitFormButton } from '@/modules/auth/components/submit-form-button'
+import { createJob } from '@/modules/job/actions/actions'
+
+interface Props {
+  categories: string[]
+}
+
+const initialState = {
+  id: 0,
+  category: '',
+  description: '',
+  image: '',
+  location: '',
+  title: '',
+  wage: 0,
+  userId: 1
+}
+
+export default function NewJobForm({ categories }: Props) {
+  const [_state, formAction] = useActionState(createJob, initialState)
+
+  const handleChange = (value: string) => {
+    // TODO: handle form data and update state with selected category
+  }
+
+  return (
+    <form action={formAction} className="card-body">
+      <FormControl label="Title" placeholder="Web Developer" type="text" />
+      <FormControl
+        label="Description"
+        placeholder="Looking for an experienced web developer to join our team."
+        type="text"
+      />
+      <FormControl label="Location" placeholder="Shenzhen" type="text" />
+      <div className="form-control">
+        <label className="label" htmlFor="Category">
+          <span className="label-text">Category</span>
+        </label>
+        <select
+          className="select select-bordered w-full"
+          value={_state.category}
+          onChange={(event) => handleChange(event.target.value)}
+        >
+          <option value="category">Select a category</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {capitalizeFirstLetter(slugify(category))}
+            </option>
+          ))}
+        </select>
+      </div>
+      <FormControl label="Wage" placeholder="100" type="text" />
+      <FormControl
+        label="Image"
+        placeholder="https://via.placeholder.com/150"
+        type="text"
+      />
+      <div className="form-control mt-6">
+        <SubmitFormButton label="Post job" />
+      </div>
+    </form>
+  )
+}
