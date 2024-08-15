ad6e244ccf1799bf1079fe219bf3bd5bf2809802
NoistNT <arielgnr23@gmail.com>
2024-08-15 14:45:25 -0300

    Add create job action

--- modules/job/actions/actions.ts
+++ modules/job/actions/actions.ts
@@ -2,7 +2,59 @@
 
 import type { Job } from '@/modules/job/types'
 
+import { z } from 'zod'
+
 import { API_URL } from '@/constants'
+
+export const createJob = async (
+  _prevState: {
+    id: number
+    category: string
+    description: string
+    image: string
+    location: string
+    title: string
+    wage: number
+  },
+  formData: FormData
+) => {
+  const schema = z.object({
+    id: z.number(),
+    category: z.string(),
+    description: z.string(),
+    image: z.string(),
+    location: z.string(),
+    title: z.string(),
+    wage: z.number()
+  })
+
+  const parse = schema.safeParse({
+    id: await getJobs().then((jobs) => jobs.length + 1),
+    category: formData.get('category'),
+    description: formData.get('description'),
+    image: formData.get('image'),
+    location: formData.get('location'),
+    title: formData.get('title'),
+    wage: Number(formData.get('wage'))
+  })
+
+  if (!parse.success) {
+    throw new Error(parse.error.issues.toString())
+  }
+
+  // TODO: Update userId with current userId
+  const job = { ...parse.data, userId: 1 }
+
+  const res = await fetch(`${API_URL}/job`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(job)
+  })
+
+  if (!res.ok) throw new Error('Failed to create job')
+
+  return job
+}
 
 export const getJobs = async (
   title?: string,
