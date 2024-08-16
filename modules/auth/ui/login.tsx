9eb91940214a40f9fcf3d4c692c18a06d0a6d89a
NoistNT <arielgnr23@gmail.com>
2024-08-16 11:09:45 -0300

    Minor style tweaks in login component

--- modules/auth/ui/login.tsx
+++ modules/auth/ui/login.tsx
@@ -12,18 +12,18 @@ export function Login() {
   return (
     <div>
       <ClerkLoading>
-        <div className="btn btn-outline hidden w-24 items-center justify-center border-blue-500 hover:border-gray-100 hover:bg-blue-500 hover:text-white hover:duration-300 hover:ease-linear lg:flex">
+        <div className="btn items-center justify-center lg:flex lg:w-24">
           <Loader className="h-5 w-5 animate-spin text-blue-500" />
         </div>
       </ClerkLoading>
       <ClerkLoaded>
         <SignedOut>
-          <div className="btn btn-outline hidden w-24 items-center justify-center border-blue-500 hover:border-gray-100 hover:bg-blue-500 hover:text-white hover:duration-300 hover:ease-linear lg:flex">
+          <div className="btn btn-outline hidden items-center justify-center border-blue-500 hover:border-gray-100 hover:bg-blue-500 hover:text-white hover:duration-300 hover:ease-linear lg:flex lg:w-24">
             <SignInButton fallbackRedirectUrl="/jobs" mode="modal" />
           </div>
         </SignedOut>
         <SignedIn>
-          <div className="btn btn-ghost w-24">
+          <div className="btn btn-ghost lg:w-24">
             <UserButton afterSwitchSessionUrl="/" />
           </div>
         </SignedIn>
